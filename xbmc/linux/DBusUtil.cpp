ddc6bdb93ddd87d2684d54a017eeafdc7c767835
belese <besme2@gmail.com>
2015-03-12 16:46:15 +0100

    Generalise Get, add Set, Call, CallAsync- base class DBusHelper"

--- xbmc/linux/DBusUtil.cpp
+++ xbmc/linux/DBusUtil.cpp
@@ -20,90 +20,205 @@
 #include "DBusUtil.h"
 #ifdef HAS_DBUS
 #include "utils/log.h"
+#include "settings/AdvancedSettings.h"
+#include <dbus/dbus.h>
 
+//TODO
+//The CDBusHelper class should be outside this file, 
+//but don't fond th e way, added in makefile.in
+//and get link error
+//It's a base class for handler and object template
+
+CDBusHelper::CDBusHelper()
+{  
+  //Get the DBUS connection
+  DBusError error;
+  dbus_error_init (&error);
+  
+  m_connection = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
+
+  if (dbus_error_is_set(&error))
+  {
+    CLog::Log(LOGERROR, "DBus: Cannot Get Dbus %s : %s", error.name, error.message);
+    m_connection = NULL;        
+  }
+  dbus_error_free (&error);
+  if (m_connection!=NULL)
+    dbus_connection_set_exit_on_disconnect(m_connection, false);
+}
+
+void CDBusHelper::ReadAndDispatchDbusMessage()
+{
+  //Process and empty Dbus queue here
+  DBusDispatchStatus status;
+     
+  status = dbus_connection_get_dispatch_status (m_connection); 
+     
+  while (status == DBUS_DISPATCH_DATA_REMAINS)
+  {
+    if (g_advancedSettings.CanLogComponent(LOGDBUS))
+      CLog::Log(LOGDEBUG, "DBus: We have waiting message, let's dispatch");
+    status = dbus_connection_dispatch(m_connection);
+  }
+     
+  if (status==DBUS_DISPATCH_NEED_MEMORY)       
+    CLog::Log(LOGERROR, "DBus: Dbus_connection_dispatch need memory");			   
+}
+
+const char* CDBusHelper::CreateRule(const char *type,const char *path,const char *iface, const char *name)
+{	
+  //TODO : probably a better way in c++
+  std::string rules = "";
+    
+  if (type)    
+    rules = StringUtils::Format("type='%s'",type);
+          
+  if (path)
+  {
+    if (!rules.empty())
+      rules+=',';
+    rules+=StringUtils::Format("path='%s'",path);
+  }
+        
+  if (iface)
+  {
+    if (!rules.empty())
+      rules+=',';   
+    rules += StringUtils::Format("interface='%s'",iface);
+  }
+        
+  if (name)
+  {
+    if (!rules.empty())
+      rules+=',';   
+    rules += StringUtils::Format("member='%s'",name);
+  }
+    
+  return rules.c_str();     
+}
+
+bool CDBusHelper::RegisterMatch(const char *type,const char *path,const char *iface, const char *name)
+{
+  DBusError error;
+  dbus_error_init (&error);   
+    
+  const char * rule = CDBusHelper::CreateRule(type,path,iface,name);
+  if (g_advancedSettings.CanLogComponent(LOGDBUS))
+    CLog::Log(LOGDEBUG, "DBus: Register Match : %s",rule);
+  
+  dbus_bus_add_match(m_connection, rule, &error);    
+    
+  if (dbus_error_is_set(&error))
+  {
+    CLog::Log(LOGERROR, "DBus: Cannot ADD %s MATCH : Path %s  Iterface : %s Member : %s",type, path, iface,name);
+    dbus_error_free (&error);
+    return false;
+  }
+    
+  dbus_error_free (&error);          
+  return true;        
+}
+
+bool CDBusHelper::UnregisterMatch(const char *type,const char *path,const char *iface, const char *name)
+{
+  DBusError error;
+  dbus_error_init (&error);   
+
+  const char * rule = CDBusHelper::CreateRule(type,path,iface,name);
+  if (g_advancedSettings.CanLogComponent(LOGDBUS))
+    CLog::Log(LOGDEBUG, "DBus: UnregisterMatch : %s",rule);
+  dbus_bus_remove_match(m_connection, rule, &error);
+	
+  if (dbus_error_is_set(&error))
+  {
+    CLog::Log(LOGERROR, "DBus: Cannot REMOVE %s MATCH : Path %s  Iterface : %s Member : %s",type, path, iface,name);     
+	dbus_error_free (&error);	
+	return false;
+  }
+	
+  dbus_error_free (&error);	
+  return true;
+}
+
+//deprecated use GET instead
 CVariant CDBusUtil::GetVariant(const char *destination, const char *object, const char *interface, const char *property)
 {
-//dbus-send --system --print-reply --dest=destination object org.freedesktop.DBus.Properties.Get string:interface string:property
-  CDBusMessage message(destination, object, "org.freedesktop.DBus.Properties", "Get");
-  CVariant result;
-
-  if (message.AppendArgument(interface) && message.AppendArgument(property))
-  {
-    DBusMessage *reply = message.SendSystem();
-
-    if (reply)
-    {
-      DBusMessageIter iter;
-
-      if (dbus_message_iter_init(reply, &iter))
-      {
-        if (!dbus_message_has_signature(reply, "v"))
-          CLog::Log(LOGERROR, "DBus: wrong signature on Get - should be \"v\" but was %s", dbus_message_iter_get_signature(&iter));
-        else
-          result = ParseVariant(&iter);
-      }
-    }
-  }
-  else
-    CLog::Log(LOGERROR, "DBus: append arguments failed");
-
-  return result;
+  return CDBusUtil::Get(destination,object,interface,property);
 }
 
-CVariant CDBusUtil::GetAll(const char *destination, const char *object, const char *interface)
+//Helper to get a dbus properties
+template <typename T>
+bool CDBusUtil::Set(const char *destination, const char *object, const char *interface, const char *property,T value)
 {
-  CDBusMessage message(destination, object, "org.freedesktop.DBus.Properties", "GetAll");
-  CVariant properties;
-  message.AppendArgument(interface);
+  return CDBusUtil::CallAsync(destination,object, "org.freedesktop.DBus.Properties", "Set",interface,property,value);
+}
+
+CVariant CDBusUtil::Get(const char *destination, const char *object, const char *interface, const char *property)
+{
+  return CDBusUtil::Call(destination,object, "org.freedesktop.DBus.Properties", "Get",interface,property);
+}
+
+CVariant CDBusUtil::GetAll(const char *destination, const char *object, const char *arg)
+{
+  return CDBusUtil::Call(destination, object, "org.freedesktop.DBus.Properties", "GetAll", arg);
+}
+
+//Call without arg
+CVariant CDBusUtil::Call(const char *destination, const char *object, const char *interface, const char *method)
+{
+	CDBusMessage message(destination, object, interface, method);
+	DBusMessage *reply = message.SendSystem();
+    if (reply)    
+	  return CDBusUtil::Decode(reply);		    
+}
+
+//Call with arg(s)
+template< typename... Args>
+CVariant CDBusUtil::Call(const char *destination, const char *object, const char *interface, const char *method, Args... args)
+{
+  CDBusMessage message(destination, object, interface, method);  
+  message.AppendArgument(args...);
   DBusMessage *reply = message.SendSystem();
-  if (reply)
-  {
-    DBusMessageIter iter;
-    if (dbus_message_iter_init(reply, &iter))
-    {
-      if (!dbus_message_has_signature(reply, "a{sv}"))
-        CLog::Log(LOGERROR, "DBus: wrong signature on GetAll - should be \"a{sv}\" but was %s", dbus_message_iter_get_signature(&iter));
-      else
-      {
-        do
-        {
-          DBusMessageIter sub;
-          dbus_message_iter_recurse(&iter, &sub);
-          do
-          {
-            DBusMessageIter dict;
-            dbus_message_iter_recurse(&sub, &dict);
-            do
-            {
-              const char * key = NULL;
-
-              dbus_message_iter_get_basic(&dict, &key);
-              if (!dbus_message_iter_next(&dict))
-                break;
-
-              CVariant value = ParseVariant(&dict);
-
-              if (!value.isNull())
-                properties[key] = value;
-
-            } while (dbus_message_iter_next(&dict));
-
-          } while (dbus_message_iter_next(&sub));
-
-        } while (dbus_message_iter_next(&iter));
-      }
-    }
-  }
-
-  return properties;
+  if (reply)    
+	return CDBusUtil::Decode(reply);		    
 }
 
-CVariant CDBusUtil::ParseVariant(DBusMessageIter *itr)
+//Call asynchronous without arg
+bool CDBusUtil::CallAsync(const char *destination, const char *object, const char *interface, const char *method)
 {
-  DBusMessageIter variant;
-  dbus_message_iter_recurse(itr, &variant);
+	CDBusMessage message(destination, object, interface, method);
+	return message.SendAsyncSystem();
+}
 
-  return ParseType(&variant);
+//Call asynchronous with arg(s)
+template< typename... Args>
+bool CDBusUtil::CallAsync(const char *destination, const char *object, const char *interface, const char *method, Args... args)
+{ 
+  CDBusMessage message(destination, object, interface, method);  
+  message.AppendArgument(args...);  
+  return message.SendAsyncSystem();
+}
+
+CVariant CDBusUtil::Decode(DBusMessage *message)
+{
+  //If only one args, return args
+  //return CVariant array of args
+  CVariant properties;
+	
+  DBusMessageIter iter;
+  
+  if (dbus_message_iter_init(message, &iter))
+  {      
+	do
+	{
+	  properties.push_back(ParseType(&iter));      
+    } while (dbus_message_iter_next(&iter));
+  }
+	  
+  if (properties.size() == 1)
+	return properties[0];
+
+  return properties;	
 }
 
 CVariant CDBusUtil::ParseType(DBusMessageIter *itr)
@@ -153,18 +268,45 @@ CVariant CDBusUtil::ParseType(DBusMessageIter *itr)
   case DBUS_TYPE_ARRAY:
     DBusMessageIter array;
     dbus_message_iter_recurse(itr, &array);
-
-    value = CVariant::VariantTypeArray;
-
-    do
-    {
-      CVariant item = ParseType(&array);
-      if (!item.isNull())
-        value.push_back(item);
-    } while (dbus_message_iter_next(&array));
+    if (dbus_message_iter_get_arg_type(&array) == DBUS_TYPE_DICT_ENTRY)
+    {    
+        //it's a dict inside this array
+        value = CVariant::VariantTypeObject;
+        DBusMessageIter dictEntry;        
+	    do
+        {
+          dbus_message_iter_recurse(&array,&dictEntry);							
+	      const char * key = NULL;
+	      dbus_message_iter_get_basic(&dictEntry, &key);
+          dbus_message_iter_next(&dictEntry);
+          CVariant val = ParseType(&dictEntry);
+          if (!val.isNull() && key)
+          {         
+             value[key] = val;
+          }
+        }while (dbus_message_iter_next(&array));
+	}
+	else
+	{
+      //standard array
+      value = CVariant::VariantTypeArray;
+      do
+      {
+        CVariant item = ParseType(&array);
+        if (!item.isNull())
+          value.push_back(item);
+      } while (dbus_message_iter_next(&array));
+    }
+    break;
+  case DBUS_TYPE_VARIANT:
+    DBusMessageIter variant;
+    dbus_message_iter_recurse(itr, &variant);
+    value = ParseType(&variant);
     break;
   }
-
+  CLog::Log(LOGINFO, "BELESE: GetValue %s", value.asString().c_str());
+  
   return value;
 }
+
 #endif
--- xbmc/linux/DBusUtil.h
+++ xbmc/linux/DBusUtil.h
@@ -22,15 +22,57 @@
 #ifdef HAS_DBUS
 #include "DBusMessage.h"
 #include "utils/Variant.h"
+#include "utils/StringUtils.h"
+#include "utils/log.h"
+
+
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <mutex>
+
+class CDBusHelper
+{    
+public : 
+  CDBusHelper();
+  //CDBusHelper(DBusConnection *connection);
+  ~CDBusHelper() {};
+  
+protected :  
+  DBusConnection* m_connection;
+  
+  void ReadAndDispatchDbusMessage();    
+
+  bool RegisterMatch(const char *type,const char *path,const char *iface, const char *name);    
+  bool UnregisterMatch(const char *type,const char *path,const char *iface, const char *name);
+private :    
+  static const char* CreateRule(const char *type,const char *path,const char *iface, const char *name); 
+};
 
 class CDBusUtil
 {
 public:
+  //**
+  //deprecated : use generic Get instead
+  static CVariant GetVariant(const char *destination, const char *object, const char *interface, const char *property);
+  //**
+  
+  template <typename T> static bool Set(const char *destination, const char *object, const char *interface, const char *property,T value);
+  
+  static CVariant Get(const char *destination, const char *object, const char *interface, const char *property);
   static CVariant GetAll(const char *destination, const char *object, const char *interface);
 
-  static CVariant GetVariant(const char *destination, const char *object, const char *interface, const char *property);
+  static CVariant Call(const char *destination, const char *object, const char *interface, const char *method);
+  template< typename... Args> static CVariant Call(const char *destination, const char *object, const char *interface, const char *method, Args ... args);
+
+  static bool CallAsync(const char *destination, const char *object, const char *interface, const char *method);
+  template< typename... Args> static bool CallAsync(const char *destination, const char *object, const char *interface, const char *method, Args ... args);
+    
+  static CVariant Decode(DBusMessage *message);
+  
 private:
   static CVariant ParseType(DBusMessageIter *itr);
-  static CVariant ParseVariant(DBusMessageIter *itr);
 };
+ 
 #endif
