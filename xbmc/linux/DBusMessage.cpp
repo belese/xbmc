3da3383527ad8381500cbe1b77b4833f7036a7eb
belese <besme2@gmail.com>
2015-03-12 16:42:58 +0100

    Extends Dbus Message to quite all Dbus type (except struc and nested array) - extend to reply, error reply and signal message

--- xbmc/linux/DBusMessage.cpp
+++ xbmc/linux/DBusMessage.cpp
@@ -21,53 +21,132 @@
 #ifdef HAS_DBUS
 #include "utils/log.h"
 #include "settings/AdvancedSettings.h"
+#include "utils/Variant.h"
+#include "DBusUtil.h"
 
 CDBusMessage::CDBusMessage(const char *destination, const char *object, const char *interface, const char *method)
 {
   m_reply = NULL;
   m_message = dbus_message_new_method_call (destination, object, interface, method);
   m_haveArgs = false;
-
+  
   if (g_advancedSettings.CanLogComponent(LOGDBUS))
     CLog::Log(LOGDEBUG, "DBus: Creating message to %s on %s with interface %s and method %s\n", destination, object, interface, method);
 }
+
+CDBusMessage::CDBusMessage(DBusMessage *replyto) 
+{
+  m_reply = NULL;
+  m_message = dbus_message_new_method_return(replyto);
+  m_haveArgs = false;
+
+  if (g_advancedSettings.CanLogComponent(LOGDBUS))
+    CLog::Log(LOGDEBUG, "DBus: Creating Reply message\n");
+}
+
+CDBusMessage::CDBusMessage(DBusMessage *replyto,const char *name, const char *message)
+{
+  m_reply = NULL;
+  m_message = dbus_message_new_error(replyto,name,message);
+  m_haveArgs = false;
+
+  if (g_advancedSettings.CanLogComponent(LOGDBUS))
+    CLog::Log(LOGDEBUG, "DBus: Creating Error Reply message %s : %s\n",name,message);
+}
+
+CDBusMessage::CDBusMessage(const char *object, const char *interface, const char *name)
+{
+  m_reply = NULL;
+  m_message = dbus_message_new_signal(object,interface,name);
+  m_haveArgs = false;
+
+  if (g_advancedSettings.CanLogComponent(LOGDBUS))
+    CLog::Log(LOGDEBUG, "DBus: Creating Signal Message on %s with interface %s  : %s\n",object,name,interface);
+}
+
+template<> 
+bool CDBusMessage::AppendArgument(const CVariant variant)
+{ 
+  bool success = true;
+  
+  if (variant.isByte())
+  {
+     const char value = variant.asByte();
+     success &= AppendVariant(&value,DBUS_TYPE_BYTE);
+  }
+  else if (variant.isInteger())
+  {
+     const int value = variant.asInteger();
+     success &= AppendVariant(&value,DBUS_TYPE_INT32);
+  }
+  else if (variant.isUnsignedInteger())
+  {
+     const unsigned int value = variant.asUnsignedInteger();
+     success &= AppendVariant(&value,DBUS_TYPE_UINT32);
+  }
+  else if (variant.isString())
+  {
+	 const char* value = variant.asString().c_str();
+     success &= AppendVariant(&value,DBUS_TYPE_STRING);
+  }
+  else if (variant.isBoolean())
+  {
+	 const bool value = variant.asBoolean();
+     success &= AppendVariant(&value,DBUS_TYPE_BOOLEAN);
+  }
+  else if (variant.isDouble())
+  {
+	 const double value = variant.asDouble();
+     success &= AppendVariant(&value,DBUS_TYPE_DOUBLE);
+  }
+  return success;  
+}
+
 
 CDBusMessage::~CDBusMessage()
 {
   Close();
 }
 
-bool CDBusMessage::AppendObjectPath(const char *object)
+const char* CDBusMessage::GetSignatureFromType(int type) 
 {
-  PrepareArgument();
-  return dbus_message_iter_append_basic(&m_args, DBUS_TYPE_OBJECT_PATH, &object);
+    switch (type) {
+        case DBUS_TYPE_BOOLEAN: return DBUS_TYPE_BOOLEAN_AS_STRING;
+        case DBUS_TYPE_BYTE: return DBUS_TYPE_BYTE_AS_STRING;
+        case DBUS_TYPE_INT16: return DBUS_TYPE_INT16_AS_STRING;
+        case DBUS_TYPE_UINT16: return DBUS_TYPE_UINT16_AS_STRING;
+        case DBUS_TYPE_INT32: return DBUS_TYPE_INT32_AS_STRING;
+        case DBUS_TYPE_UINT32: return DBUS_TYPE_UINT32_AS_STRING;
+        case DBUS_TYPE_INT64: return DBUS_TYPE_INT64_AS_STRING;
+        case DBUS_TYPE_UINT64: return DBUS_TYPE_UINT64_AS_STRING;
+        case DBUS_TYPE_DOUBLE: return DBUS_TYPE_DOUBLE_AS_STRING;
+        case DBUS_TYPE_STRING: return DBUS_TYPE_STRING_AS_STRING;
+        case DBUS_TYPE_OBJECT_PATH: return DBUS_TYPE_OBJECT_PATH_AS_STRING;
+        case DBUS_TYPE_SIGNATURE: return DBUS_TYPE_SIGNATURE_AS_STRING;        
+        default : return NULL;
+    }
 }
 
-bool CDBusMessage::AppendArgument(const char *string)
-{
-  PrepareArgument();
-  return dbus_message_iter_append_basic(&m_args, DBUS_TYPE_STRING, &string);
-}
-
-bool CDBusMessage::AppendArgument(const bool b)
-{
-  dbus_bool_t arg = (b == true);
-  PrepareArgument();
-  return dbus_message_iter_append_basic(&m_args, DBUS_TYPE_BOOLEAN, &arg);
-}
-
-bool CDBusMessage::AppendArgument(const char **arrayString, unsigned int length)
+bool CDBusMessage::OpenContainer(const int type,const char *signature)
 {
   PrepareArgument();
   DBusMessageIter sub;
-  bool success = dbus_message_iter_open_container(&m_args, DBUS_TYPE_ARRAY, DBUS_TYPE_STRING_AS_STRING, &sub);
-
-  for (unsigned int i = 0; i < length && success; i++)
-    success &= dbus_message_iter_append_basic(&sub, DBUS_TYPE_STRING, &arrayString[i]);
-
-  success &= dbus_message_iter_close_container(&m_args, &sub);
-
+  bool success = dbus_message_iter_open_container(&m_currentItr,type, signature , &sub);
+  m_itr.push_back(m_currentItr);
+  m_currentItr = sub;
   return success;
+}
+
+bool CDBusMessage::CloseContainer()
+{
+  if (m_itr.empty())
+	return false;
+  
+  DBusMessageIter sub;
+  sub = m_currentItr;
+  m_currentItr = m_itr.back();
+  m_itr.pop_back();
+  return dbus_message_iter_close_container(&m_currentItr,&sub);
 }
 
 DBusMessage *CDBusMessage::SendSystem()
@@ -96,10 +175,19 @@ DBusMessage *CDBusMessage::Send(DBusBusType type)
   dbus_error_init (&error);
   DBusConnection *con = dbus_bus_get(type, &error);
 
+  if (dbus_error_is_set(&error))
+  {
+    CLog::Log(LOGERROR, "DBus: Cannot Get Dbus : %s - %s", error.name, error.message);
+    dbus_error_free (&error);  
+    return false;
+  }
+  
+  dbus_error_init (&error);
+  
   DBusMessage *returnMessage = Send(con, &error);
 
   if (dbus_error_is_set(&error))
-    CLog::Log(LOGERROR, "DBus: Error %s - %s", error.name, error.message);
+    CLog::Log(LOGERROR, "DBus: Error Cannot send message %s - %s", error.name, error.message);
 
   dbus_error_free (&error);
   dbus_connection_unref(con);
@@ -130,7 +218,7 @@ DBusMessage *CDBusMessage::Send(DBusConnection *con, DBusError *error)
   {
     if (m_reply)
       dbus_message_unref(m_reply);
-
+      
     m_reply = dbus_connection_send_with_reply_and_block(con, m_message, -1, error);
   }
 
@@ -147,9 +235,11 @@ void CDBusMessage::Close()
 }
 
 void CDBusMessage::PrepareArgument()
-{
+{    
   if (!m_haveArgs)
-    dbus_message_iter_init_append(m_message, &m_args);
+  {   
+    dbus_message_iter_init_append(m_message, &m_currentItr);    
+  }  
 
   m_haveArgs = true;
 }
--- xbmc/linux/DBusMessage.h
+++ xbmc/linux/DBusMessage.h
@@ -21,36 +21,189 @@
 #include "system.h"
 #ifdef HAS_DBUS
 #include <dbus/dbus.h>
+#include <vector>
+#include <map>
+#include "utils/Variant.h"
+#include "utils/StringUtils.h"
+#include "utils/log.h"
 
 class CDBusMessage
 {
 public:
-  CDBusMessage(const char *destination, const char *object, const char *interface, const char *method);
+  //DBUS Call Method Message
+  CDBusMessage(const char *destination, const char *object, const char *interface, const char *method);  
+  //DBUS Call Reply Message
+  CDBusMessage(DBusMessage *replyto);  
+  //DBUS Call Reply Error Message
+  CDBusMessage(DBusMessage *replyto,const char *name, const char *message);  
+  //DBUS Signal Message
+  CDBusMessage(const char *object, const char *interface, const char *name);
+    	
   ~CDBusMessage();
-
-  bool AppendObjectPath(const char *object);
-  bool AppendArgument(const char *string);
-  bool AppendArgument(const bool b);
-  bool AppendArgument(const char **arrayString, unsigned int length);
 
   DBusMessage *SendSystem();
   DBusMessage *SendSession();
-
+  
   bool SendAsyncSystem();
   bool SendAsyncSession();
-
+  
+  //Theses ones are let public for compatibility
+  //but shouldn't be use in public context
   DBusMessage *Send(DBusBusType type);
   DBusMessage *Send(DBusConnection *con, DBusError *error);
+    
+  template<typename... T> bool AppendArgument(const T... args);
+  template<typename T> bool AppendArgument(const T arg);
+  
+  //use recursively for variatic template
+  template <typename T,typename... U> bool AppendArgument(T t,U... args);    
+  //use recursively for variatic template and array args  
+  template<typename T, typename... U> bool AppendArgument(const T *array, unsigned int length,U...args);  
+  
+  template<typename T> bool AppendArgument(const T *array, unsigned int length);
+  template<typename K, typename V> bool AppendArgument(const std::map<K,V> *map);  
+  
+              
 private:
-
+  static const char* GetSignatureFromType(int type);
+    
+  static const int GetDbusTypeFromArg(const char *string) {return DBUS_TYPE_STRING;};
+  static const int GetDbusTypeFromArg(const unsigned char byte) {return DBUS_TYPE_BYTE;};
+  static const int GetDbusTypeFromArg(const bool boolean) {return DBUS_TYPE_BOOLEAN;};
+  static const int GetDbusTypeFromArg(const int16_t integer16) {return DBUS_TYPE_INT16;};  
+  static const int GetDbusTypeFromArg(const int32_t integer32) {return DBUS_TYPE_INT32;};  
+  static const int GetDbusTypeFromArg(const int64_t  integer64) {return DBUS_TYPE_INT64;};
+  static const int GetDbusTypeFromArg(const uint16_t uinteger16) {return DBUS_TYPE_UINT16;};
+  static const int GetDbusTypeFromArg(const uint32_t uinteger32) {return DBUS_TYPE_UINT32;};  
+  static const int GetDbusTypeFromArg(const uint64_t uinteger64) {return DBUS_TYPE_UINT64;};
+  static const int GetDbusTypeFromArg(const double vdouble) {return DBUS_TYPE_DOUBLE;};
+  
   bool SendAsync(DBusBusType type);
 
   void Close();
   void PrepareArgument();
 
+  bool OpenContainer(const int type,const char *signature);
+  bool CloseContainer();
+  
+  bool isRoot();
+    
+  template<typename T> bool AppendBasic(int type,const T arg);  
+  template<typename T> bool AppendArray(const T *array, unsigned int length,int type);
+  template<typename K, typename V> bool AppendDictEntry(const K *key,const V *value);       
+  template<typename K, typename V> bool AppendDict(const std::map<K,V> *map,int keytype,int valuetype);
+  template<typename U> bool AppendVariant(const U *value,int type);
+  
   DBusMessage *m_message;
   DBusMessage *m_reply;
-  DBusMessageIter m_args;
+  DBusMessageIter m_currentItr;
+  
+  std::vector<DBusMessageIter> m_itr;
   bool m_haveArgs;
 };
+
+template< typename... T> 
+bool CDBusMessage::AppendArgument(const T... args)
+{
+	return AppendArgument(args...);
+}
+
+template<typename T> 
+bool CDBusMessage::AppendArgument(const T arg)
+{
+  T value = arg;
+  return AppendBasic(CDBusMessage::GetDbusTypeFromArg(arg),(T*)&value);
+}
+ 
+template<typename K, typename V> 
+bool CDBusMessage::AppendArgument(const std::map<K,V> *map)
+{
+	K keytype;
+	V valuetype;
+	return AppendDict(map,CDBusMessage::GetDbusTypeFromArg(keytype),CDBusMessage::GetDbusTypeFromArg(valuetype));
+}
+
+
+template <typename T,typename... U>
+bool CDBusMessage::AppendArgument(T t,U... args) 
+{
+  bool success = AppendArgument(t);
+  success &= AppendArgument(args...); 
+  return success; 
+}
+
+template<typename T, typename... U> 
+bool CDBusMessage::AppendArgument(const T *array, unsigned int length,U...args)
+{
+  bool success = AppendArgument(array,length);
+  success &= AppendArgument(args...);
+  return success;
+}
+
+template<typename T> 
+bool CDBusMessage::AppendArgument(const T *array, unsigned int length)
+{
+	T arraytype;
+	return AppendArray(array,length,CDBusMessage::GetDbusTypeFromArg(arraytype));
+}
+
+
+template<typename T> 
+bool CDBusMessage::AppendBasic(int type,const T value)
+{
+  PrepareArgument();  
+  bool rc = dbus_message_iter_append_basic(&m_currentItr, type, value);
+  if (!rc)
+     CLog::Log(LOGERROR, "DBus: Cannot Basic AppendArgument %d %s",type, *value);
+  return rc;
+} 
+
+template<typename T> 
+bool CDBusMessage::AppendArray(const T *array, unsigned int length,int type)
+{
+  PrepareArgument();
+  bool success = OpenContainer(DBUS_TYPE_ARRAY,GetSignatureFromType(type));
+  for (unsigned int i = 0; i < length && success; i++)
+  {    
+      success &=AppendArgument(array[i]);   
+  }  
+  success &=CloseContainer();
+
+  return success;
+}
+
+template<typename K, typename V>
+bool CDBusMessage::AppendDict(const std::map<K,V> *map,int keytype,int valtype)
+{
+  PrepareArgument();
+  std::string signature = StringUtils::Format("a{%s%s}",GetSignatureFromType(keytype),GetSignatureFromType(valtype));
+  bool success = OpenContainer(DBUS_TYPE_ARRAY,signature.c_str());
+  for (typename std::map<K,V>::const_iterator it=map->begin(); it!=map->end(); ++it)
+  {
+		success &= AppendDictEntry(it->first.c_str(),&it->second);
+  }	
+  success &= CloseContainer();
+  return success;	  
+}
+
+template<typename K,typename V> 
+bool CDBusMessage::AppendDictEntry(const K *key,const V *data)
+{
+	bool success = OpenContainer(DBUS_TYPE_DICT_ENTRY,NULL);
+	success &= AppendArgument(key);	
+    success &= AppendArgument(data);    
+	success &= CloseContainer();
+	return success;
+}
+
+template<typename T> 
+bool CDBusMessage::AppendVariant(const T *value,int type)
+{
+  PrepareArgument();
+  bool success = OpenContainer(DBUS_TYPE_VARIANT,GetSignatureFromType(type));
+  success &= AppendArgument(value);
+  success &= CloseContainer();
+  return success;
+}
+
 #endif
